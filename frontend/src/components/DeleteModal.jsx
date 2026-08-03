d0968f839af2246f0bb028c3a23cfbb2b398a64c
Your Name <your_email@example.com>
2026-08-03 08:28:14 +0530

    Add reusable delete confirmation modal

---
+++ frontend/src/components/DeleteModal.jsx
@@ -0,0 +1,51 @@
+﻿import "../styles/deleteModal.css";
+
+function DeleteModal({
+
+    open,
+    title="Delete Video",
+    message="Are you sure?",
+    onCancel,
+    onConfirm
+
+}){
+
+    if(!open) return null;
+
+    return(
+
+        <div className="modal-overlay">
+
+            <div className="modal-box">
+
+                <h2>{title}</h2>
+
+                <p>{message}</p>
+
+                <div className="modal-actions">
+
+                    <button
+                        className="cancel-btn"
+                        onClick={onCancel}
+                    >
+                        Cancel
+                    </button>
+
+                    <button
+                        className="delete-btn"
+                        onClick={onConfirm}
+                    >
+                        Delete
+                    </button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default DeleteModal;
