f6d5842b3933e92dc76b0b575f38062fb2328189
Your Name <your_email@example.com>
2026-08-03 06:56:09 +0530

    Complete video CRUD update and delete APIs

--- backend/src/controllers/videoController.js
+++ backend/src/controllers/videoController.js
@@ -8,7 +8,6 @@ export const createVideo = async (req, res) => {
         });
 
         res.status(201).json(video);
-
     } catch (error) {
         res.status(500).json({
             message: error.message
@@ -16,30 +15,25 @@ export const createVideo = async (req, res) => {
     }
 };
 
-
 export const getVideos = async (req, res) => {
     try {
-
         const videos = await Video.find()
             .populate("owner", "username")
             .populate("channel", "name");
 
         res.json(videos);
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
 };
 
-
 export const getVideoById = async (req, res) => {
     try {
-
-        const video = await Video.findById(req.params.id);
+        const video = await Video.findById(req.params.id)
+            .populate("owner", "username")
+            .populate("channel", "name");
 
         if (!video) {
             return res.status(404).json({
@@ -48,25 +42,18 @@ export const getVideoById = async (req, res) => {
         }
 
         video.views += 1;
-
         await video.save();
 
         res.json(video);
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
 };
-
 
 export const updateVideo = async (req, res) => {
-
     try {
-
         const video = await Video.findById(req.params.id);
 
         if (!video) {
@@ -75,90 +62,52 @@ export const updateVideo = async (req, res) => {
             });
         }
 
-
-        if (
-            video.owner.toString() !== req.user._id.toString()
-        ) {
+        if (video.owner.toString() !== req.user._id.toString()) {
             return res.status(403).json({
                 message: "Not authorized"
             });
         }
 
-
-        video.title =
-            req.body.title ?? video.title;
-
-        video.description =
-            req.body.description ?? video.description;
-
-        video.videoUrl =
-            req.body.videoUrl ?? video.videoUrl;
-
-        video.thumbnailUrl =
-            req.body.thumbnailUrl ?? video.thumbnailUrl;
-
-        video.category =
-            req.body.category ?? video.category;
-
+        video.title = req.body.title ?? video.title;
+        video.description = req.body.description ?? video.description;
+        video.videoUrl = req.body.videoUrl ?? video.videoUrl;
+        video.thumbnailUrl = req.body.thumbnailUrl ?? video.thumbnailUrl;
+        video.category = req.body.category ?? video.category;
 
         await video.save();
 
-
         res.json(video);
-
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
 
-
 export const deleteVideo = async (req, res) => {
-
     try {
-
         const video = await Video.findById(req.params.id);
 
-
         if (!video) {
-
             return res.status(404).json({
                 message: "Video not found"
             });
-
         }
 
-
-        if (
-            video.owner.toString() !== req.user._id.toString()
-        ) {
-
+        if (video.owner.toString() !== req.user._id.toString()) {
             return res.status(403).json({
                 message: "Not authorized"
             });
-
         }
 
-
         await video.deleteOne();
-
 
         res.json({
             message: "Video deleted"
         });
-
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
