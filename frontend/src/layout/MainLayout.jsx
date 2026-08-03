1573fc1b8b8d830939a6f87ca980fbd329c1a084
Your Name <your_email@example.com>
2026-08-03 08:25:30 +0530

    Implement YouTube-style application layout

---
+++ frontend/src/layout/MainLayout.jsx
@@ -0,0 +1,31 @@
+﻿import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import "../styles/layout.css";
+
+function MainLayout({ children }) {
+
+    return (
+
+        <>
+
+            <Navbar />
+
+            <div className="app-layout">
+
+                <Sidebar />
+
+                <main className="app-content">
+
+                    {children}
+
+                </main>
+
+            </div>
+
+        </>
+
+    );
+
+}
+
+export default MainLayout;
